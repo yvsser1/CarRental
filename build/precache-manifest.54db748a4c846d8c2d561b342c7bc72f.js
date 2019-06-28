069e3b07424134b6733c1d65d5f9e19d2ad11975
Yasser Dalouzi <yvsser1@gmail.com>
2019-06-28 20:43:57 +0100

    Production Build

---
+++ build/precache-manifest.54db748a4c846d8c2d561b342c7bc72f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1ecae242dfaf6d69e92cf4bbc29a3cd",
+    "url": "/static/media/home_bg.e1ecae24.png"
+  },
+  {
+    "revision": "b31625e9c317a9a192063bf2d7e30fbc",
+    "url": "/static/media/home-hero.b31625e9.jpg"
+  },
+  {
+    "revision": "8c726c677d52cbb67d0f42bf98b89224",
+    "url": "/static/media/bg-cars.8c726c67.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec3410989bf3b4fb74d4",
+    "url": "/static/js/main.6af54b01.chunk.js"
+  },
+  {
+    "revision": "caa09f38606c7e28ece5",
+    "url": "/static/js/2.ba37ddb7.chunk.js"
+  },
+  {
+    "revision": "ec3410989bf3b4fb74d4",
+    "url": "/static/css/main.119e6db3.chunk.css"
+  },
+  {
+    "revision": "caa09f38606c7e28ece5",
+    "url": "/static/css/2.19b208aa.chunk.css"
+  },
+  {
+    "revision": "c8babfdd85c96828bad75193910d07b6",
+    "url": "/index.html"
+  }
+];
--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
-var express = require('express');
-
-var app = express()
-
-app.use(express.static('public'));
-
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
